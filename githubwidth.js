e15137d709d09e048458a91c26d355c9f74f1f44
Chris Culy <chris.culy@logos.com>
2014-01-29 08:51:01 -0800

    Truncate file names only when the full available space is used up.

--- githubwidth.js
+++ githubwidth.js
@@ -45,7 +45,13 @@ function setNiceWidths() {
 		if (typeof elements[index].style != 'undefined')
 			elements[index].style.width = shouldAdjustPage ? '100%' : '';
 	}
-	
+
+	elements = document.getElementsByClassName('css-truncate');
+	for (var index in elements)
+	{
+		if (typeof elements[index].style != 'undefined')
+			elements[index].style.maxWidth = shouldAdjustPage ? '100%' : ''; 
+	}
 	
 	var mainWidth = getWidth('repo-container');
 	var sidebarWidth = getWidth('repository-sidebar');
